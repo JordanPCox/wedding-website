abe965860b4cfa84d51d16ff19ba255c0a12edd6
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-02-11 12:56:42 -0500

    Added a footer

--- views/default.jsx
+++ views/default.jsx
@@ -4,13 +4,16 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
+                <title>Wedding Website</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
             <main className="container">
                 <body>
                     {html.children}
+                    <footer>
+                        This is the sticky footer
+                    </footer>
                 </body>
             </main>
         </html>
